62bd1b8ccf4089b58052406ead82a4abbecaef62
Tanaka <f.tanaka@gftd.works>
2022-06-16 13:03:04 +0900

    pugモジュールによるテンプレートを導入

---
+++ index.js
@@ -0,0 +1,64 @@
+'use strict';
+const http = require('http');
+
+//pugモジュールの読み込み
+const pug = require('pug');
+const server = http
+  .createServer((req, res) => {
+    const now = new Date();
+    console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
+
+    switch (req.method) {
+      case 'GET':
+
+        //URLによって、表示されるpugモジュールによるテンプレート内の値が変わるようにする描画
+        if(req.url==='/enquetes/yaki-shabu') {
+
+          //描画時にpugテンプレートに埋め込みたい値を、オブジェクト形式で渡す
+          res.write(pug.renderFile('./form.pug', {
+            path: req.url,
+            firstItem: '焼き肉',
+            secondItem: 'しゃぶしゃぶ'
+          }));
+        } else if (req.url==='/enquetes/rice-bread') {
+          
+           //描画時にpugテンプレートに埋め込みたい値を、オブジェクト形式で渡す
+            res.write(pug.renderFile('./form.pug', {
+            path: req.url,
+            firstItem: 'ごはん',
+            secondItem: 'パン'
+          }));
+        }
+        res.end();
+        break;
+      case 'POST':
+        let rawData = '';
+        req
+          .on('data', chunk => {
+            rawData += chunk;
+          })
+          .on('end', () => {
+            const answer = new URLSearchParams(rawData);
+            const body = `${answer.get('name')}さんは${answer.get('favorite')}に投票しました`;
+            console.info(`[${now}] ${body}`);
+            res.write(`<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`);
+            res.end();
+          });
+        break;
+      default:
+        break;
+    }
+  })
+  .on('error', e => {
+    console.error(`[${new Date()}] Server Error`, e);
+  })
+  .on('clientError', e => {
+    console.error(`[${new Date()}] Client Error`, e);
+  });
+const port = 8000;
+server.listen(port, () => {
+  console.info(`[${new Date()}] Listening on ${port}`);
+});
